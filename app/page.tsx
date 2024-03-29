1acc408554ef1e8d60e28d5df0f6c00a97dfc40d
vicky <debugentity9@gmail.com>
2024-03-29 16:44:17 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import Banner from '@/components/Banner'
+import Clients from '@/components/Clients'
+import Contact from '@/components/Contact'
+import Hero from '@/components/Hero'
+import OurBreads from '@/components/OurBreads'
+import OurMenus from '@/components/OurMenus'
+import WhyUs from '@/components/WhyUs'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <Hero />
+      <OurBreads />
+      <WhyUs />
+      <OurMenus />
+      <Banner />
+      <Clients />
+      <Contact />
+    </>
+  )
+}
+
+export default page
